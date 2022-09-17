285c94cb6a1417992611f09d7304d8c127c86303
b-erhart <contact@b-erhart.de>
2022-09-17 13:24:05 +0200

    Add some proper styling with Bulma Lumen theme

--- src/templates/base.php
+++ src/templates/base.php
@@ -6,18 +6,38 @@
 
         <title><?=$siteTitle?> - Simple Scores</title>
 
-        <link rel="stylesheet" href="/assets/css/new.css"/>
+        <link rel="stylesheet" href="/assets/css/bulmaswatch-lumen.min.css"/>
         <link rel="icon" type="image/x-icon" href="./assets/images/favicon.ico"/>
     </head>
     <body>
-        <header>
-            <nav>
-                <h3>Simple Scores</h3>
-                <a href="/">Home</a> /
-                <a href="/bundesliga">1. Bundesliga</a> /
-                <a href="/nfl">NFL</a>
-            </nav>
-        </header>
+        <nav class="navbar is-primary" role="navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="/"><strong>Simple Scores</strong></a>
+                <a class="navbar-burger" id="navbar-burger-icon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </a>
+            </div>
+            <div class="navbar-menu navbar-end" id="navbar-menu">
+                <a class="navbar-item" href="/">Home</a>
+                <a class="navbar-item" href="/bundesliga">Bundesliga</a>
+                <a class="navbar-item" href="/nfl">NFL</a>
+            </div>
+            <script>
+                const navbarBurgerIcon = document.querySelector("#navbar-burger-icon");
+                const navbarMenu = document.querySelector("#navbar-menu");
+
+                navbarBurgerIcon.addEventListener('click', () => {
+                    navbarBurgerIcon.classList.toggle('is-active');
+                    navbarMenu.classList.toggle('is-active');
+                });
+            </script>
+        </nav>
+        <section class="section">
+            <div class="container">
 <?php include($content) ?>
+            </div>
+        </section>
     </body>
 </html>
--- src/templates/nfl.php
+++ src/templates/nfl.php
@@ -6,9 +6,48 @@
 <?php endif; ?>
 <?php endforeach; ?>
         </select>
-        <h1 style="text-align: center">NFL</h1>
-        <p><?=$currentMatchDay->GroupName?>:</p>
+        <h1 class="title">NFL</h1>
+        <h2 class="subtitle"><?=$currentMatchDay->GroupName?></h2>
+<?php $previousTime = 0; ?>
 <?php foreach ($currentMatches as $match): ?>
 <?php $dateTime = strtotime($match->MatchDateTime); ?>
-        <p><?=$match->Team2->TeamName?> @ <?=$match->Team1->TeamName?></p>
+<?php if (date('l, j. F Y', $previousTime) != date('l, j. F Y', $dateTime)): ?>
+        <p class="heading"><?=date('l, j. F Y', $dateTime);?></p>
+<?php endif; ?>
+        <div class="box level">
+            <div class="level-left has-text-centered">
+<?php if ($match->MatchIsFinished): ?>
+                <p>Final</p>
+<?php else: ?>
+                <p><?=date('H:i', $dateTime);?> CEST</p>
+<?php endif; ?>
+            </div>
+            <div class="level-item level has-text-centered">
+                <div class="level-right">
+                    <div>
+                        <p>
+                            <figure class="image is-32x32" style="margin: 0 8px;">
+                                <img src="<?=$match->Team2->TeamIconUrl?>" style="border-radius: 5%;"/>
+                            </figure>
+                            <?=array_slice(explode(' ', trim($match->Team2->TeamName)), -1)[0];?>
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <p>@</p>
+                </div>
+                <div class="level-left">
+                    <div>
+                        <figure class="image is-32x32" style="margin: 0 8px;">
+                            <img src="<?=$match->Team1->TeamIconUrl?>" style="border-radius: 5%;"/>
+                        </figure>
+                        <p><?=array_slice(explode(' ', trim($match->Team1->TeamName)), -1)[0];?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="level-right has-text-centered">
+                <a href="#">Details</a>
+            </div>
+        </div>
+<?php $previousTime = $dateTime; ?>
 <?php endforeach; ?>

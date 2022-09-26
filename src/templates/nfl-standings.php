bf6b23c8eea4dbbd3f0eebed717328905ab8cf94
b-erhart <contact@b-erhart.de>
2022-09-26 17:41:55 +0200

    Improve standings-layout for small screens

--- src/templates/nfl-standings.php
+++ src/templates/nfl-standings.php
@@ -18,11 +18,12 @@
 <?php $i = 1; $rank = 1; $previousRecord = ''; foreach ($division->entries as $entry): ?>
                     <tr>
                         <td><?=($previousRecord != $entry->record) ? $rank = $i : $rank?><?=($entry->divisionRivalHasEqualRecord) ? '<a href="#tie-break-hint">*</a>' : ''?></td>
-                        <td class="standingscard-icon-column">
+                        <td><div class="standingscard-team" style="background: <?=$entry->teamColor?>"><?=$entry->team?></div></td>
+                        <!--<td class="standingscard-icon-column">
                             <div class="standingscard-team-color" style="background: <?=$entry->teamColor?>"></div>
                             <?=$entry->team?>
 
-                        </td>
+                        </td>-->
                         <td><?=$entry->record?></td>
                         <td><?=$entry->points?></td>
                         <td><?=$entry->netPoints?></td>

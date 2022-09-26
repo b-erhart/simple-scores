9e2f7429d599197601071100bae724961088eaee
b-erhart <contact@b-erhart.de>
2022-09-26 17:46:52 +0200

    Further improve standings-layout for different screen sizes

--- src/templates/nfl-standings.php
+++ src/templates/nfl-standings.php
@@ -19,11 +19,6 @@
                     <tr>
                         <td><?=($previousRecord != $entry->record) ? $rank = $i : $rank?><?=($entry->divisionRivalHasEqualRecord) ? '<a href="#tie-break-hint">*</a>' : ''?></td>
                         <td><div class="standingscard-team" style="background: <?=$entry->teamColor?>"><?=$entry->team?></div></td>
-                        <!--<td class="standingscard-icon-column">
-                            <div class="standingscard-team-color" style="background: <?=$entry->teamColor?>"></div>
-                            <?=$entry->team?>
-
-                        </td>-->
                         <td><?=$entry->record?></td>
                         <td><?=$entry->points?></td>
                         <td><?=$entry->netPoints?></td>

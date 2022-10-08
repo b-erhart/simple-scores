4e33425e2ceae17b757670c5ca5ea258027e913b
b-erhart <contact@b-erhart.de>
2022-10-08 14:45:18 +0200

    Change line endings to unix and add PoC guzzle exception handling

--- templates/nfl-scores.php
+++ templates/nfl-scores.php
@@ -7,6 +7,7 @@
             </nav>
         </header>
         <main>
+<?php if (!isset($webServiceException)): ?>
             <select id="week" onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
 <?php foreach($allMatchweekMetas as $matchweekMeta): ?>
                 <option <?=($selectedWeek == $matchweekMeta->getInSeasonId()) ? 'selected ' : ''?>value="<?='/nfl/' . $matchweekMeta->getInSeasonId()?>"><?=$matchweekMeta->getName()?><?=($matchweekMeta->getInSeasonId() == $currentMatchweekInSeasonId) ? ' (now)' : ''?></option>
@@ -29,4 +30,8 @@
 <?php endforeach; ?>
             </div>
 <?php endforeach; ?>
+<?php else: ?>
+            <p>Unable to retrieve web service data. Please try again later.</p>
+            <p>Developer information: <code><?=$webServiceException->getMessage()?></code></p>
+<?php endif; ?>
         </main>

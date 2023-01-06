2c7b69bc569eb486cd7bbef99c02506d938985cc
b-erhart <contact@b-erhart.de>
2023-01-06 20:58:48 +0100

    Reset project for full rewrite

--- templates/home.php
+++
@@ -1,6 +0,0 @@
-        </header>
-        <main>
-            <h1>Home</h1>
-            <p>Welcome to Simple Scores - the lightweight website for your Bundesliga and NFL results!</p>
-            <p>Use the navigation menu at the top of the page to get the results for your league.</p>
-        </main>
